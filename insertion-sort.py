0370a61fffdbd065db3808546ea0eb0bc5746ff4
samieee <87869105+sam22ridhi@users.noreply.github.com>
2023-06-20 23:50:36 +0530

    insertion-sort.py

--- insertion-sort.py
+++ insertion-sort.py
@@ -1,1 +1,12 @@
-
+def insertionsort(L):
+    n = len(L)
+    if n < 1:
+        return(L)
+    for i in range(n):
+        j = i
+        while(j > 0 and L[j] < L[j-1]):
+            (L[j],L[j-1]) = (L[j-1],L[j])
+            j = j-1
+    return(L)
+L = [5,6,3,2,4,1]
+print(insertionsort(L))
