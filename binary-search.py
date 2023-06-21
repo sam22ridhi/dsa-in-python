259793ddb420a4d8f04124e4b0151fdf4c95b583
samieee <87869105+sam22ridhi@users.noreply.github.com>
2023-06-21 11:46:29 +0530

    Update binary-search.py

--- binary-search.py
+++ binary-search.py
@@ -1,1 +1,15 @@
-
+def binarysearch(L,v):
+  low=0
+  high=len(L)-1
+  while low<=high:
+    mid=(low+high)//2
+    if L[mid]<v:
+      low=mid+1
+    elif L[mid]>v:
+      high=mid-1
+    else:
+      return mid
+  return False
+L = [1,3,4,5,6,8]
+v = 6
+print(binarysearch(L,v))
