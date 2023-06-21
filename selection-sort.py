82c8b2128a791d88623c3b08b9b526cece13470f
samieee <87869105+sam22ridhi@users.noreply.github.com>
2023-06-21 11:22:23 +0530

    Update selection-sort.py

--- selection-sort.py
+++ selection-sort.py
@@ -1,1 +1,14 @@
 
+def selectionsort(L):
+  n=len(L)
+  if n<1:
+    return(L)
+  for i in range(n):
+    minpos=i
+    for j in range(i+1,n):
+      if L[j]<=L[minpos]:
+        minpos=j
+    (L[i],L[minpos])=(L[minpos],L[i])
+  return(L)
+L= [4,5,2,1,3]
+print(selectionsort(L))
